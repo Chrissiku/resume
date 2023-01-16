bf7c35793d036f684539b2a567a197ee68f736ed
Chris Siku <chrissiku5@gmail.com>
2023-01-16 13:40:12 +0200

    Create profile section

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 import "./style.css";
+import avatar from "./public/avatar.jpg";
 
 document.querySelector("#app").innerHTML = `
-  <h1> Christian Siku </h1>`;
+  <div class="container">
+  <div class="profile">
+  <div class="profile_container">
+    <div class="profile_image">
+      <img src='${avatar}' alt="Christian Siku">
+    </div>
+    <div>
+      <h1 class="profile_name">
+        <span class="first_name">Christian</span>
+        <span class="last_name">Siku</span>
+      </h1>
+      <p class="profile_title">Software Developer</p>
+      <p class="description profile_description">
+      As a software developer, I am dedicated to increasing the accessibility of open source technologies, creating tools that empower individuals, and fostering a sense of community. My areas of expertise include utilizing technologies such as React, Next, TypeScript, Redux, Ruby, Ruby on Rails, JavaScript, PHP, APIs, PostgreSQL, and MySQL.
+      </p>
+      <a class="download_Button" href=#" download="#">Download Resume</a>
+    </div>
+  </div>
+</div>
+  </div>`;
